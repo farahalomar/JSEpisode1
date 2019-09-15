6b07771218f2df57bd05f6b423b5caf991e88649
Farah AlOmar <farahjalomar@gmail.com>
2019-09-15 11:34:25 +0300

    Farah

--- functions.js
+++ functions.js
@@ -5,7 +5,10 @@
  * - logs "Hello <name>" if there is a name
  */
 function greet(name) {
-  // Your code here
+  if (name)
+  console.log(`Hello ${name}`);
+  else 
+  console.log(`Hello`);
 }
 
 /**
@@ -14,7 +17,7 @@ function greet(name) {
  * - returns true if it's odd, false otherwise
  */
 function isOdd(n) {
-  // Your code here
+  return !!(n%2);
 }
 
 /**
@@ -29,7 +32,9 @@ function isOdd(n) {
  * Hint: you can solve this without writing any loops!
  */
 function oddsSmallerThan(n) {
-  // Your code here
+  if (isOdd(n)) n--;
+  return n/2;
+ 
 }
 
 /**
@@ -43,7 +48,10 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+if (isOdd(n)){
+  return(n**2);
+}else
+return (n*2)
 }
 
 /**
@@ -59,13 +67,21 @@ function squareOrDouble(n) {
  * Hint: you can use JavaScript's Date() class to make things easier
  *
  * e.g. (if we assume that the date today is September 23 2018):
- *    ageFromBirthDate("19870506") -> 31 (because the birthday already passed)
- *    ageFromBirthDate("19971110") -> 20 (because the birthday hasn't passed yet)
+ *    ageFromBirthDate(19870506) -> 31 (because the birthday already passed)
+ *    ageFromBirthDate(19971110) -> 20 (because the birthday hasn't passed yet)
  */
 function ageFromBirthDate(birthDate) {
   const _MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365;
 
   // Your code here
+  const year = birthDate.slice(0,4);
+  const month = birthDate.slice(4,6);
+  const day = birthDate.slice(6,8);
+  const today = new Date();
+  const dob = new Date(`${month}/${day}/${year}`);
+  let age= today - dob
+  age = age/_MS_PER_YEAR;
+  return Math.floor(age);
 }
 
 module.exports = {
